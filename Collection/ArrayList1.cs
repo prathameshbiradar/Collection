102700b546e92c2971fddcd5e0e57950eec9a4ea
prathmeshbiradar <psbiradar948@gmail.com>
2024-10-16 17:12:57 +0530

    Adding collection programs

---
+++ Collection/ArrayList1.cs
@@ -0,0 +1,100 @@
+﻿/*
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    internal class ArrayLists
+    {
+        static void Main(String[] args)
+        { 
+            ArrayList list = new ArrayList();
+            list.Add("zossjj");
+            list.Add("Prathmesh");
+            list.Add("Tushar");
+            
+            string secondElement = (string)list[1];
+            var firstElement = list[0];
+           
+            Console.WriteLine($"First element is : {firstElement} and second element is: {secondElement} \n");
+            
+            list[0] = "Akash";
+            
+
+            foreach (var i in list)
+            {
+            Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            var arraylist = new ArrayList() {
+            "Akash",
+            102,
+            "Pune"
+            };
+            //foreach (var a in arraylist)
+            //{
+            //    Console.WriteLine(a);
+            //}
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(arraylist[i]);
+            }
+            Console.WriteLine();
+
+            ArrayList list2 = new ArrayList() {
+            101,"Om","Pune",422012
+            };
+
+            list2.Insert(1, "Yash");
+            foreach (var i in list2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+            list.InsertRange(1, list2);
+            foreach (var b in list)
+            {
+                Console.WriteLine(b);
+            }
+            Console.WriteLine();
+
+            var list3 = new ArrayList() {
+            45,"Rohit","Mumbai","Batsman",45,"Mumbai",123
+            };
+            list3.Remove(45);
+            list3.RemoveAt(1);
+            list3.RemoveRange(4, 1);
+            foreach (var i in list3)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            bool isPresent= list3.Contains("Rohit");
+            Console.WriteLine(isPresent);
+            Console.WriteLine();
+
+            ArrayList cloneArray = (ArrayList)list3.Clone();
+            foreach (var i in cloneArray)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //coping arraylist in array using CopyTO()method.
+
+            object[] arr = new object[list3.Count];
+            list3.CopyTo(arr);
+            foreach (var i in arr) { 
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
+*/
---
+++ Collection/LinkedList1.cs
@@ -0,0 +1,194 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    public class Node
+    {
+        public int data;
+        public Node next;
+
+        public Node(int data)
+        { 
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public class LinkedList1
+    {
+        public static void traverseLinkedList(Node head)
+        { 
+            Node current = head;
+            while (current != null)
+            {
+                Console.WriteLine(current.data+" ");
+                current = current.next;
+            }
+            Console.WriteLine();
+        
+        }
+        public Boolean searchLinkedList(Node head, int target)
+        {
+            while (head != null)
+            {
+                if (head.data == target)
+                {
+                    return true;
+                }
+                head = head.next;
+
+            }
+            return false;
+        }
+        public int lengthLinkedList(Node head)
+        {
+            int length = 0;
+            Node current = head;
+            while (current != null)
+            {
+                length++;
+                current = current.next;
+                
+            }
+            return length;
+        }
+        public static Node InsertAtBeagining(Node head,int value)
+        {
+            Node newNode = new Node(value);
+
+            newNode.next = head;
+            head = newNode;
+            return head;
+
+        }
+        public static Node InsertAtLast(Node head, int value)
+        { 
+            Node newnode = new Node(value);
+            if (head == null)
+            { 
+                return newnode;
+            }
+            Node current = head;
+            while (current.next != null)
+            { 
+                current = current.next;
+            }
+            current.next = newnode;
+            return head;
+        }
+        public static Node insertPos(Node head, int pos, int value)
+        {
+
+            if (pos < 1) {
+                Console.WriteLine("Invalid Position");
+                return head;    
+            }
+            if (pos == 1)
+            {
+                Node tem = new Node(value);
+                tem.next = head;
+                return tem;
+            }
+
+            Node prev = head;
+            int count = 1;
+            while (count < pos-1 && prev !=null)
+            {
+                prev = prev.next;
+                count++;
+
+            }
+            if (prev == null)
+            {
+                Console.WriteLine("Invalid Position");
+                return head;
+            }
+            Node temp = new Node(value);
+            temp.next = prev.next;
+            prev.next = temp;
+            return head;
+        }
+        public static Node DeleteAtFirstPosition(Node head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+            Node temp = head;
+            head = head.next;
+            return head;
+        }
+        public static Node DeleteAtLastPosition(Node head)
+        {
+            if (head == null)
+            { 
+                return null;
+            }
+            if (head.next == null)
+            {
+                head = null;
+                return null;
+            }
+            Node second_last = head;
+            while (second_last.next.next != null) { 
+                second_last= second_last.next;
+            }
+            second_last.next = null;
+            return head;
+        }
+        public static void deleteAtposition(Node head, int posi)
+        {
+
+            if (head == null || posi < 1) {
+                return;
+            }
+            if (posi == 1) {
+                Node tem = head;
+                head = head.next;
+                tem = null;
+                return;
+            }
+            Node current = head;
+            int count = 1;
+            while (count < posi - 1 && current.next != null)
+            {
+                current = current.next;
+                count++;
+            }
+            Node temp = current.next;
+            current.next = current.next.next;
+            temp = null;
+        
+        }
+        
+        static void Main(String[]args)
+        {
+            Node head = null; // Initialize the head of the linked list
+
+            // Insert elements at the beginning
+            
+            head = LinkedList1.InsertAtBeagining(head, 10);
+            head = LinkedList1.InsertAtBeagining(head, 20);
+            head = LinkedList1.InsertAtBeagining(head, 30);
+         
+
+            Console.WriteLine("Linked List after inserting at beginning:");
+            LinkedList1.traverseLinkedList(head);
+
+            head = LinkedList1.InsertAtLast(head, 40);
+            LinkedList1.traverseLinkedList(head);
+
+        }
+        
+        
+
+
+    }
+}
+*/
